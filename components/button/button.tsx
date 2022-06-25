a4f4aaf8c0bd185d3be4bdef432b28808a837bd0
于天翔 <1143048093@qq.com>
2022-06-26 02:35:49 +0800

    -

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const Button = (props: any) => {
-  return <button className="my-button-test">{props.children}</button>;
-};
+interface IButton {
+    className?: string
+    type?: "text" | "contained" | "outlined"
+    children?: React.ReactElement | React.ReactElement[] | string | string[] | number | number[];
+    onClick?: () => void
+}
+
+const Button = React.forwardRef<HTMLButtonElement, IButton>((props, ref) => {
+    return <button
+        ref={ref}
+        className={`perfect-button-${props.type}${props.className && ' ' + props.className}`}
+        onClick={props.onClick}
+    >
+        {props.children}
+    </button>;
+})
+
+Button.defaultProps = {
+    type: "contained",
+    className: "",
+    onClick: () => {
+    }
+}
 
 export default Button;
